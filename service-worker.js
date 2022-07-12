c845234ae2093bf86742fad7c613d3aea3de07ca
asasinmode <63049433+asasinmode@users.noreply.github.com>
2022-07-12 19:31:34 +0200

    'deploy'

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,12 @@ const CURRENT_CACHES = {
    images: 'image-cache-v' + CACHE_VERSION
 };
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', (event) => {
    const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
+      caches.keys().then((cacheNames) => {
          return Promise.all(
-         cacheNames.map(function(cacheName) {
+         cacheNames.map((cacheName) => {
             if (!expectedCacheNamesSet.has(cacheName)) {
                console.log('Deleting out of date cache:', cacheName);
                return caches.delete(cacheName);
@@ -20,38 +20,24 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-   console.log('Handling fetch event for', event.request.url);
-
-   event.respondWith(
-      caches.open(CURRENT_CACHES.images).then(function(cache) {
-         return cache.match(event.request).then(function(response) {
-            if (response) {
-               console.log(' Found response in cache:', response);
-
-               return response;
-            }
-
-            console.log(' No response for %s found in cache. About to fetch from network...', event.request.url);
-
-            return fetch(event.request.clone()).then(function(response) {
-               console.log('  Response for %s from network is: %O', event.request.url, response);
-
-               if (response.status < 400 &&
-                  response.headers.has('content-type') &&
-                  response.headers.get('content-type').match(/^image\//i)) {
-                  console.log('  Caching the response to', event.request.url);
-                  cache.put(event.request, response.clone());
-               } else {
-                  console.log('  Not caching the response to', event.request.url);
-               }
-
-               return response;
-            });
-         }).catch(function(error) {
-            console.error('  Error in fetch handler:', error);
-
-            throw error;
-         });
-      })
-   );
+   if(event.request.destination === "image"){
+      console.log('Handling fetch event for', event.request.url);
+      event.respondWith(
+         caches.open(CURRENT_CACHES.images).then(async cache => {
+            const response = await cache.match(event.request).then(response => {
+               return response || fetch(event.request).then(fetchResponse => {
+                  console.log("fetching from network, no response found in cache")
+                  if(fetchResponse.status < 400){
+                     cache.put(event.request, fetchResponse)
+                  }
+                  return fetchResponse
+               })
+            })
+            return response
+         }).catch(error => {
+            console.error(error)
+            throw error
+         })
+      )
+   }
 });
