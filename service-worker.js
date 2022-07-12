5e55367989f0702f37e0818a17375624d413c814
asasinmode <63049433+asasinmode@users.noreply.github.com>
2022-07-12 20:15:01 +0200

    'deploy'

--- service-worker.js
+++ service-worker.js
@@ -7,13 +7,12 @@ self.addEventListener('activate', (event) => {
    const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
    event.waitUntil(
       caches.keys().then((cacheNames) => {
-         return Promise.all(
-         cacheNames.map((cacheName) => {
-            if (!expectedCacheNamesSet.has(cacheName)) {
-               console.log('Deleting out of date cache:', cacheName);
-               return caches.delete(cacheName);
-            }
-         })
+         return Promise.all(cacheNames.map((cacheName) => {
+               if (!expectedCacheNamesSet.has(cacheName)) {
+                  console.log('Deleting out of date cache:', cacheName);
+                  return caches.delete(cacheName);
+               }
+            })
          );
       })
    );
@@ -21,26 +20,35 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', function(event) {
    if(event.request.destination === "image"){
-      console.log(event.request.url.slice(-8), 'handling fetch event for');
       event.respondWith(
-         caches.open(CURRENT_CACHES.images).then(async cache => {
-            const response = await cache.match(event.request).then(response => {
-               console.log(event.request.url.slice(-8), "cache match response", response)
-               return response || fetch(event.request).then(fetchResponse => {
-                  console.log(event.request.url.slice(-8), "fetching from network, no response found in cache")
-                  if(fetchResponse.status < 400){
-                     console.log(event.request.url.slice(-8), "putting response in cache", fetchResponse)
-                     cache.put(event.request, fetchResponse)
-                  }
-                  return fetchResponse
-               })
+         caches.match(event.request).then(response => {
+            if(!response){console.log("attempting to fetch", event.request)}
+            return response || fetch(event.request).then(fetchResponse => {
+               console.log("successfully fetched, putting in cache and returning", fetchResponse)
+               caches.put(event.request, fetchResponse)
+               return fetchResponse
             })
-            console.log(event.request.url.slice(-8), "returning from awaiting cache.match", response)
-            return response
-         }).catch(error => {
-            console.error(error)
-            throw error
          })
       )
+      // event.respondWith(
+      //    caches.open(CURRENT_CACHES.images).then(async cache => {
+      //       const response = await cache.match(event.request).then(response => {
+      //          console.log(event.request.url.slice(-8), "cache match response", response)
+      //          return response || fetch(event.request).then(fetchResponse => {
+      //             console.log(event.request.url.slice(-8), "fetching from network, no response found in cache")
+      //             if(fetchResponse.status < 400){
+      //                console.log(event.request.url.slice(-8), "putting response in cache", fetchResponse)
+      //                cache.put(event.request, fetchResponse)
+      //             }
+      //             return fetchResponse
+      //          })
+      //       })
+      //       console.log(event.request.url.slice(-8), "returning from awaiting cache.match", response)
+      //       return response
+      //    }).catch(error => {
+      //       console.error(error)
+      //       throw error
+      //    })
+      // )
    }
 });
