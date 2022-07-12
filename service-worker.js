faf0132f887ccf2d23963c9d4ad19edd8d60dc34
asasinmode <63049433+asasinmode@users.noreply.github.com>
2022-07-12 19:43:08 +0200

    'deploy'

--- service-worker.js
+++ service-worker.js
@@ -21,18 +21,21 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', function(event) {
    if(event.request.destination === "image"){
-      console.log('Handling fetch event for', event.request.url);
+      console.log(event.request.url.slice(-8), 'handling fetch event for');
       event.respondWith(
          caches.open(CURRENT_CACHES.images).then(async cache => {
             const response = await cache.match(event.request).then(response => {
+               console.log(event.request.url.slice(-8), "cache match response", response)
                return response || fetch(event.request).then(fetchResponse => {
-                  console.log("fetching from network, no response found in cache")
+                  console.log(event.request.url.slice(-8), "fetching from network, no response found in cache")
                   if(fetchResponse.status < 400){
+                     console.log(event.request.url.slice(-8), "putting response in cache", fetchResponse)
                      cache.put(event.request, fetchResponse)
                   }
                   return fetchResponse
                })
             })
+            console.log(event.request.url.slice(-8), "returning from awaiting cache.match", response)
             return response
          }).catch(error => {
             console.error(error)
